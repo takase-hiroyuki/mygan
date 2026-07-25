c9a23a6aeea17a215c71f26a2812eb4b66615819
TAKASE Hiroyuki <takase-hiroyuki@users.noreply.github.com>
2026-07-26 08:49:08 +0900

    Update Supabase key in config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 // 1. Supabaseの接続設定
 export const SUPABASE_URL = "https://hpuvozteepfhttxiqnvl.supabase.co";
-export const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlhc2R2emZzd2tmc3RzY3NkbGZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMwODAxMDAsImV4cCI6MjA5ODY1NjEwMH0.TaRlLAUAO3aQI9a4hlveImr1z1WqbQYhSPorltqKnwM";
+export const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhwdXZvenRlZXBmaHR0eGlxbnZsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ5MjY3MjksImV4cCI6MjEwMDUwMjcyOX0.QjLb1XGlPncslKzQ-gtk052bcuPzD4QPbb0eJ-Dttk4";
 
 // 2. 小規模運用のための固定設定
 export const roomId = "room01"; // ゲーム（部屋）は1つに固定
