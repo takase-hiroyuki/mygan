c75320b7d7216944c216cd6743f012356f4f9e0b
TAKASE Hiroyuki <takase-hiroyuki@users.noreply.github.com>
2026-08-02 21:16:46 +0900

    Delete index_ui_cards_utils.js

--- index_ui_cards_utils.js
+++
@@ -1,9 +0,0 @@
-// index_ui_cards_utils.js
-import { SEL_G } from './common_dom_selectors.js';
-
-export function getLocalPlayerName() {
-    const nameEl = document.getElementById(SEL_G.STATUS.NAME);
-    return (nameEl && nameEl.textContent !== '未定') ? nameEl.textContent : 'プレイヤー';
-}
-
-console.log("【デバッグ】index_ui_cards_utils.js が読み込まれました。");
